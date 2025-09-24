fe681b3502447504dfbcb8698706246164e7c5a0
Shane T. Geary <shane.geary@glowstik.com>
2025-09-24 09:38:30 -0600

    Read input file as a string, parse and create list, loop through list and add unique words to dictionary as keys that increment the integer value upon each instance of the original key

--- Assignments/hw4_Geary.py
+++ Assignments/hw4_Geary.py
@@ -1,44 +1,25 @@
+# Shane T Geary
+# CINF: 108
+# 09/24/2025
+# University at Albany
+
+import string
+
 # Create file variable and open Alice (Alice.txt) in Wonderland 
-
- 
-
- 
-
- 
+aliceInWonderland = open('Alice.txt', 'r')
 
 # Create file variable and open WordCount.txt for writing
-
-#  wordcount = open("AliceWordCount.txt", 'w')
-
- 
-
- 
-
- 
+wordcount = open("AliceWordCount.txt", 'w')
 
 # Read contents of Alice.txt into a string variable
-
+aliceIWString = aliceInWonderland.read()
  
-
- 
-
- 
-
 # Create a list from the string variable and covert all text to lower-case
-
- 
-
- 
-
- 
+aliceIWList = aliceIWString.lower().split()
 
 # Create a dictionary variable -- note your dictionary will have 'words' as    #  keys and an integer as a value (which will be the number of words seen in #  the file)
 
- 
-
- 
-
- 
+aliceIWWordCount = {}
 
 # loop through each word in your list and
 
@@ -46,22 +27,19 @@
 
 #   2) if already in the dictionary, increment key pair by 1
 
- 
+for word in aliceIWList:
+	if word not in aliceIWWordCount:
+		aliceIWWordCount[word] = 1
+	else:
+		aliceIWWordCount[word] += 1
+		
+print(aliceIWWordCount['about'])
+# Now, Loop through each word and write it to your output file in the format #  specified Note that your dictionary contains some non alphabetic 'words
 
- 
 
- 
-
-# Now, Loop througth each word and write it to your output file in the format #  specified Note that your dictionary contains some non alphabetic 'words
 
 #  before writing to the file, check first if it is a legitimate word.  
 
 #  Hint: use isalpha()
-
- 
-
- 
-
- 
 
 # Close both the input and output files
