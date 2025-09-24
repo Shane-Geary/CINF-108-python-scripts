ce6098d9a2036587f37a5fee28b378ec218bd4db
Shane T. Geary <shane.geary@glowstik.com>
2025-09-24 08:18:18 -0600

    Assignment 4—Instructor provided boiler-plate code

---
+++ Assignments/hw4_Geary.py
@@ -0,0 +1,67 @@
+# Create file variable and open Alice (Alice.txt) in Wonderland 
+
+ 
+
+ 
+
+ 
+
+# Create file variable and open WordCount.txt for writing
+
+#  wordcount = open("AliceWordCount.txt", 'w')
+
+ 
+
+ 
+
+ 
+
+# Read contents of Alice.txt into a string variable
+
+ 
+
+ 
+
+ 
+
+# Create a list from the string variable and covert all text to lower-case
+
+ 
+
+ 
+
+ 
+
+# Create a dictionary variable -- note your dictionary will have 'words' as    #  keys and an integer as a value (which will be the number of words seen in #  the file)
+
+ 
+
+ 
+
+ 
+
+# loop through each word in your list and
+
+#   1) if it is not in the dictionary, add it with the key pair = 1
+
+#   2) if already in the dictionary, increment key pair by 1
+
+ 
+
+ 
+
+ 
+
+# Now, Loop througth each word and write it to your output file in the format #  specified Note that your dictionary contains some non alphabetic 'words
+
+#  before writing to the file, check first if it is a legitimate word.  
+
+#  Hint: use isalpha()
+
+ 
+
+ 
+
+ 
+
+# Close both the input and output files
