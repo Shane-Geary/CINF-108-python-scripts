9ddefd2a3c011a384ccdc335de9805029d8391eb
Shane T. Geary <shane.geary@glowstik.com>
2025-09-25 06:57:16 -0600

    Python script to read from an input file of text, parse each word, count each instance of each word, and write the results to a formatted output file

--- Assignments/hw4_Geary.py
+++ Assignments/hw4_Geary.py
@@ -3,8 +3,6 @@
 # 09/24/2025
 # University at Albany
 
-import string
-
 # Create file variable and open Alice (Alice.txt) in Wonderland 
 aliceInWonderland = open('Alice.txt', 'r')
 
@@ -22,9 +20,7 @@ aliceIWList = aliceIWString.lower().split()
 aliceIWWordCount = {}
 
 # loop through each word in your list and
-
 #   1) if it is not in the dictionary, add it with the key pair = 1
-
 #   2) if already in the dictionary, increment key pair by 1
 
 for word in aliceIWList:
@@ -34,12 +30,37 @@ for word in aliceIWList:
 		aliceIWWordCount[word] += 1
 		
 print(aliceIWWordCount['about'])
+
 # Now, Loop through each word and write it to your output file in the format #  specified Note that your dictionary contains some non alphabetic 'words
-
-
-
 #  before writing to the file, check first if it is a legitimate word.  
-
 #  Hint: use isalpha()
 
+# Create columns for the output file's format
+columns = [
+	'Word\t\t\t\t',
+	'Count\n',
+	'\t\t\t\t\t|\n',
+	'-------------------- -----\n'
+]
+
+# Write columns to the output file
+for column in columns:
+	wordcount.write(column)
+
+# Initialize empty rows list before looping through dictionary
+rows = []
+for key, value in aliceIWWordCount.items():
+	if str(key).isalpha():
+		rows.append(
+			f'{key:<20}'
+			f' {value}\n'
+		)
+
+# Sort alphabetically before writing rows to output file
+rows.sort()
+for row in rows:
+	wordcount.write(row)
+
 # Close both the input and output files
+aliceInWonderland.close()
+wordcount.close()
