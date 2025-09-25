63d7dced572bfb323d60e6712a5ce4b270fd4334
Shane T. Geary <shane.geary@glowstik.com>
2025-09-25 17:01:49 -0600

    Find most followed Japanese channel to return info on it

--- Discussions/D5_Geary.py
+++ Discussions/D5_Geary.py
@@ -1,13 +1,25 @@
 # Python script to read from a CSV file with data on top Twitch channels and find specified information
-import pandas
-
-dataFrame = pandas.read_csv('twitchdata.csv')
+import csv
 
 def findTwitchChannelData():
-	# print(dataFrame.head())
-	largestFollowers = dataFrame.nlargest(1, 'Followers')
+	with open('twitchdata.csv', newline="") as file:
+		reader = csv.DictReader(file)
 
-	print(largestFollowers['Channel'].min())
+		japanChannels = []
+		for row in reader:
+			if(row["Language"] == 'Japanese'):
+				# print(row)
+				japanChannels.append(row)
 
+		# for channel in japanChannels:
+		# 	print(channel['Followers'])
+			# followers.append(channel['Followers'])
+
+		mostFollowedJapanChannel = max(
+			japanChannels,
+			key=lambda row: int(row['Followers'])
+		)
+		
+		print(mostFollowedJapanChannel['Average viewers'])
 
 findTwitchChannelData()
